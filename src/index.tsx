8762f6a900515a2afd6e592da4d1c4d7e7350467
yohainjug <yohainjug@gmail.com>
2021-01-03 00:54:50 +0200

    start working on the soutin

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,24 @@
 import * as React from 'react';
+import * as vis from 'vis-network';
+import { useImmer } from "use-immer";
+
+
 
 interface IOptimizedGraph {
-  name?: string;
+  graph: any;
+  id?: string;
+  style?: React.CSSProperties;
+  getNetwork?: () => void;
+  getNodes?: () => void;
+  getEdges?: () => void;
+  options: vis.Options;
 }
-const OptimizedGraph: React.FC<IOptimizedGraph> = ({ name }) => {
-  return <div>testing for lib changes: {name}</div>;
+const OptimizedGraph: React.FC<IOptimizedGraph> = ({ options }) => {
+  const [divRef, setDivRef] = React.useRef();
+  // const edges
+  React.useEffect(() => {
+    
+  }, [])
+  return <div>testing for lib changes: {}</div>;
 }
 export default OptimizedGraph;
